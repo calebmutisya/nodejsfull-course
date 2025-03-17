42e7a8a741098ef1244bd440a5da74bb3fa62125
calebmutisya <mutisyacalebmusau@gmail.com>
2025-03-17 15:05:40 +0300

    Ponted directory and indexhtml

--- chapter3/src/server.js
+++ chapter3/src/server.js
@@ -1,7 +1,23 @@
 import express from 'express'
+//Allow us to look for index.html
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = 5000
+
+// Get the file path from the url of the current module
+const __filename = fileURLToPath(import.meta.url)
+// Get the directory name from the file path
+const __dirname = dirname(__filename)
+
+//Serves html file from Public directory
+// Tell express to serveall filles ffrom public folder as static assets
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
